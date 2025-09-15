ebd595aa2850ca2f0d76fe4dbd299c2ee0b090ef
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-15 09:12:00 -0300

    Agrego refactorización de la clase 'Libro', para mejorar encapsulamiento

--- fi.uba.ar.cb100/src/semana03/ejercicios/nivel02/biblioteca/Libro.java
+++ fi.uba.ar.cb100/src/semana03/ejercicios/nivel02/biblioteca/Libro.java
@@ -1,25 +1,32 @@
 package semana03.ejercicios.nivel02.biblioteca;
 
+import semana03.ejercicios.utils.Validaciones;
+
+import java.util.Objects;
+
 public class Libro {
     private String titulo;
     private String autor;
     private int anioPublicacion;
-    private int copiasDisponibles;
 
-    public Libro(String titulo, String autor) {
-        this.titulo = titulo;
-        this.autor = autor;
+    public Libro(String titulo, String autor, int anioPublicacion) {
+        this.setTitulo(titulo);
+        this.setAutor(autor);
+        this.setAnioPublicacion(anioPublicacion);
     }
 
-    public Libro(String titulo, String autor, int anioPublicacion, int copiasDisponibles) throws Exception {
-        this(titulo, autor);
-        this.anioPublicacion = anioPublicacion;
-        this.setCopiasDisponibles(copiasDisponibles);
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Libro libro)) return false;
+        return anioPublicacion == libro.anioPublicacion && Objects.equals(titulo, libro.titulo) && Objects.equals(autor, libro.autor);
     }
 
-    public Boolean estaDisponible() {
-        return this.copiasDisponibles > 0;
+    @Override
+    public int hashCode() {
+        return Objects.hash(titulo, autor, anioPublicacion);
     }
+
+    // --- Getters y Setters ---
 
     public String getTitulo() {
         return titulo;
@@ -29,20 +36,24 @@ public class Libro {
         return autor;
     }
 
-    public int getCopiasDisponibles() {
-        return copiasDisponibles;
+    public int getAnioPublicacion() {
+        return anioPublicacion;
     }
 
-    public void setCopiasDisponibles(int copiasDisponibles) throws Exception {
-        if (copiasDisponibles < 0) {
-            throw new Exception("El número de copias disponibles debe ser mayor o igual a cero.");
-        }
-        this.copiasDisponibles = copiasDisponibles;
+    private void setTitulo(String titulo) {
+        Validaciones.validarNotNull(titulo, "El título de un libro debe ser no nulo");
+        Validaciones.validarNotBlank(titulo, "El título de un libro debe ser no vacío");
+        this.titulo = titulo;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Libro libro)) return false;
-        return this.getTitulo().equals(libro.getTitulo()) && this.getAutor().equals(libro.getAutor());
+    private void setAutor(String autor) {
+        Validaciones.validarNotNull(autor, "El autor de un libro debe ser no nulo");
+        Validaciones.validarNotBlank(autor, "El autor de un libro debe ser no vacío");
+        this.autor = autor;
+    }
+
+    private void setAnioPublicacion(int anioPublicacion) {
+        Validaciones.validarNumeroMayorACero(anioPublicacion, "El año de publicación de un libro debe ser mayor a");
+        this.anioPublicacion = anioPublicacion;
     }
 }
