6f180979c06f17afba8740f436f77c1716a0dceb
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-15 11:09:46 -0300

    Agrego conversión de la clase 'Libro' a registro para volverla inmutable

--- fi.uba.ar.cb100/src/semana03/ejercicios/nivel02/biblioteca/Libro.java
+++ fi.uba.ar.cb100/src/semana03/ejercicios/nivel02/biblioteca/Libro.java
@@ -4,15 +4,13 @@ import semana03.ejercicios.utils.Validaciones;
 
 import java.util.Objects;
 
-public class Libro {
-    private String titulo;
-    private String autor;
-    private int anioPublicacion;
-
-    public Libro(String titulo, String autor, int anioPublicacion) {
-        this.setTitulo(titulo);
-        this.setAutor(autor);
-        this.setAnioPublicacion(anioPublicacion);
+public record Libro(String titulo, String autor, int anioPublicacion) {
+    public Libro {
+        Validaciones.validarNotNull(titulo, "El título de un libro debe ser no nulo");
+        Validaciones.validarNotBlank(titulo, "El título de un libro debe ser no vacío");
+        Validaciones.validarNotNull(autor, "El autor de un libro debe ser no nulo");
+        Validaciones.validarNotBlank(autor, "El autor de un libro debe ser no vacío");
+        Validaciones.validarNumeroMayorACero(anioPublicacion, "El año de publicación de un libro debe ser mayor a");
     }
 
     @Override
@@ -24,36 +22,5 @@ public class Libro {
     @Override
     public int hashCode() {
         return Objects.hash(titulo, autor, anioPublicacion);
-    }
-
-    // --- Getters y Setters ---
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public int getAnioPublicacion() {
-        return anioPublicacion;
-    }
-
-    private void setTitulo(String titulo) {
-        Validaciones.validarNotNull(titulo, "El título de un libro debe ser no nulo");
-        Validaciones.validarNotBlank(titulo, "El título de un libro debe ser no vacío");
-        this.titulo = titulo;
-    }
-
-    private void setAutor(String autor) {
-        Validaciones.validarNotNull(autor, "El autor de un libro debe ser no nulo");
-        Validaciones.validarNotBlank(autor, "El autor de un libro debe ser no vacío");
-        this.autor = autor;
-    }
-
-    private void setAnioPublicacion(int anioPublicacion) {
-        Validaciones.validarNumeroMayorACero(anioPublicacion, "El año de publicación de un libro debe ser mayor a");
-        this.anioPublicacion = anioPublicacion;
     }
 }
