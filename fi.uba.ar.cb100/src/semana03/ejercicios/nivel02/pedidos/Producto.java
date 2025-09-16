50e8f2aefaf7e1b194b09dc11a481752653cceb2
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-16 09:53:32 -0300

    Agrego el registro 'Producto' para garantizar inmutabilidad y encapsulamiento

--- fi.uba.ar.cb100/src/semana03/ejercicios/nivel02/pedidos/Producto.java
+++ fi.uba.ar.cb100/src/semana03/ejercicios/nivel02/pedidos/Producto.java
@@ -2,60 +2,25 @@ package semana03.ejercicios.nivel02.pedidos;
 
 import semana03.ejercicios.utils.Validaciones;
 
-public class Producto {
-    private String codigo;
-    private String nombre;
-    private double precio;
+import java.util.Objects;
 
-    public Producto(String codigo, String nombre, double precio) {
-        setCodigo(codigo);
-        setNombre(nombre);
-        setPrecio(precio);
-    }
-
-    public boolean esMasCaroQue(Producto otro) {
-        return this.getPrecio() > otro.getPrecio();
+public record Producto(String codigo, String nombre, double precio) {
+    public Producto {
+        Validaciones.validarNotNull(codigo, "El código de un producto debe ser no nulo");
+        Validaciones.validarNotBlank(codigo, "El código de un producto debe ser no vacío");
+        Validaciones.validarNotNull(nombre, "El nombre de un producto debe ser no nulo");
+        Validaciones.validarNotBlank(nombre, "El nombre de un producto debe ser no vacío");
+        Validaciones.validarNumeroMayorACero(precio, "El precio de un producto debe ser mayor a cero");
     }
 
     @Override
-    public String toString() {
-        return "Producto{" +
-                "codigo='" + codigo + '\'' +
-                ", nombre='" + nombre + '\'' +
-                ", precio= $" + precio +
-                '}';
+    public boolean equals(Object o) {
+        if (!(o instanceof Producto producto)) return false;
+        return Objects.equals(codigo, producto.codigo);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Producto producto)) return false;
-        return this.getCodigo().equals(producto.getCodigo());
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setCodigo(String codigo) {
-        Validaciones.validarNotBlank(codigo, "El código de un producto no puede ser vacío!");
-        this.codigo = codigo;
-    }
-
-    public void setNombre(String nombre) {
-        Validaciones.validarNotBlank(nombre, "El nombre de un producto no puede ser vacío!");
-        this.nombre = nombre;
-    }
-
-    public void setPrecio(double precio) {
-        Validaciones.validarNumeroMayorACero(precio, "El precio de un producto debe ser mayor a cero!");
-        this.precio = precio;
+    public int hashCode() {
+        return Objects.hashCode(codigo);
     }
 }
