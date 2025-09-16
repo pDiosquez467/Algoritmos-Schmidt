5f6ffd917ffca79b226e4e1b62dc21c765576965
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-16 13:01:37 -0300

    Agrego cambio de nombre de 'CursoException' a 'MatriculaException' para mejorar declaratividad

---
+++ fi.uba.ar.cb100/src/semana03/ejercicios/nivel02/cursos/excepciones/MatriculaException.java
@@ -0,0 +1,7 @@
+package semana03.ejercicios.nivel02.cursos.excepciones;
+
+public class MatriculaException extends Exception{
+    public MatriculaException(String message) {
+        super(message);
+    }
+}
