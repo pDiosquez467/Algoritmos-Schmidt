a6b22f22a37f29833751746bde2124557f5a4c8e
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-22 09:17:04 -0300

    Elimino la clase 'BarajaException'

--- fi.uba.ar.cb100/src/semana03/ejercicios/nivel03/cartas/excepciones/BarajaException.java
+++
@@ -1,7 +0,0 @@
-package semana03.ejercicios.nivel03.cartas.excepciones;
-
-public class BarajaException extends Exception{
-    public BarajaException(String message) {
-        super(message);
-    }
-}
