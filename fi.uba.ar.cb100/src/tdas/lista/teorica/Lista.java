51713621fa0274319436f742f36237b9ac29a9fb
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-25 16:45:31 -0300

    Agrego método 'addLast' de Lista simplemente enlazada

---
+++ fi.uba.ar.cb100/src/tdas/lista/teorica/Lista.java
@@ -0,0 +1,32 @@
+package tdas.lista.teorica;
+
+public class Lista<T> {
+
+    private NodoSimplementeEnlazado<T> primero;
+    private NodoSimplementeEnlazado<T> cursor;
+    private int size = 0;
+
+    public Lista() {
+        this.primero = null;
+        this.cursor  = null;
+        this.size = 0;
+    }
+
+    public void addLast(T element) {
+        NodoSimplementeEnlazado<T> node = new NodoSimplementeEnlazado<>(element);
+        if (primero == null) {
+            primero = node;
+        } else {
+            NodoSimplementeEnlazado<T> current = primero;
+            while (primero.tieneProximo()) {
+                current = current.getProximo();
+            }
+            current.setProximo(node);
+        }
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+}
