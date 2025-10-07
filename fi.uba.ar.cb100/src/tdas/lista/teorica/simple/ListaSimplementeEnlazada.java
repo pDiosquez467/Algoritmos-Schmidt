6e67412f29ef7cd863e685e9f3d850ee2285fbee
pdiosquez467 <pablodiosquez467@gmail.com>
2025-10-07 08:00:46 -0300

    Agrego impl sin documentar del método 'add'

--- fi.uba.ar.cb100/src/tdas/lista/teorica/simple/ListaSimplementeEnlazada.java
+++ fi.uba.ar.cb100/src/tdas/lista/teorica/simple/ListaSimplementeEnlazada.java
@@ -1,82 +1,143 @@
 package tdas.lista.teorica.simple;
 
-public class ListaSimplementeEnlazada<T> {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-    private NodoSimplementeEnlazado<T> primero;
-    private NodoSimplementeEnlazado<T> cursor;
-    private int size = 0;
+public class ListaSimplementeEnlazada<T> implements List<T> {
+    private NodoSimplementeEnlazado<T> head;
+    private int size;
 
     public ListaSimplementeEnlazada() {
-        this.primero = null;
-        this.cursor  = null;
-        this.size = 0;
+        this.head = null;
+        this.size  = 0;
     }
 
-    public boolean add(T element) {
-        addLast(element);
-        return true;
+    @Override
+    public int size() {
+        return this.size;
     }
 
-    public void add(int index, T element) {
-        if ((index < 0) || (index > size)) {
-            throw new IndexOutOfBoundsException("Out of bounds!");
-        }
-
-        NodoSimplementeEnlazado<T> nuevo = new NodoSimplementeEnlazado<>(element);
-        if (primero == null) {
-            primero = nuevo;
-        } else {
-            NodoSimplementeEnlazado<T> current = primero;
-            for (int i = 0; i < index - 1; i++) {
-                current = current.getProximo();
-            }
-            nuevo.setProximo(current.getProximo());
-            current.setProximo(nuevo);
-        }
-        size++;
-    }
-
-    public void addLast(T element) {
-        NodoSimplementeEnlazado<T> node = new NodoSimplementeEnlazado<>(element);
-        if (primero == null) {
-            primero = node;
-        } else {
-            NodoSimplementeEnlazado<T> current = primero;
-            while (primero.tieneProximo()) {
-                current = current.getProximo();
-            }
-            current.setProximo(node);
-        }
-        size++;
-    }
-
-    public T get(int index) {
-        if ((index < 0) || (index > size)) {
-            throw new IndexOutOfBoundsException("Out of bounds!");
-        }
-        NodoSimplementeEnlazado<T> current = primero;
-        for (int i = 0; i < index; i++) {
-            current = current.getProximo();
-        }
-        return current.getDato();
-    }
-
-    public boolean contains(T key) {
-        NodoSimplementeEnlazado<T> current = primero;
-        while (current != null) {
-            if (current.getDato().equals(key)) {
-                return true;
-            }
-            current = current.getProximo();
-        }
-        return false;
-    }
-
+    @Override
     public boolean isEmpty() {
         return this.size == 0;
     }
 
-    public int size() {
-        return size;
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] t1s) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        NodoSimplementeEnlazado<T> node = new NodoSimplementeEnlazado<>(t);
+        if (this.isEmpty()) {
+            this.head = node;
+        } else {
+            NodoSimplementeEnlazado<T> current = this.head;
+            while (current.next() != null) {
+                current = current.next();
+            }
+            current.setNext(node);
+        }
+        this.size++;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends T> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        this.head = null;
+        this.size  = 0;
+    }
+
+    @Override
+    public T get(int i) {
+        return null;
+    }
+
+    @Override
+    public T set(int i, T t) {
+        return null;
+    }
+
+    @Override
+    public void add(int i, T t) {
+
+    }
+
+    @Override
+    public T remove(int i) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int i) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int i, int i1) {
+        return List.of();
     }
 }
