2604fd52412d61d0fec83aca20ae6675047522fc
pdiosquez467 <pablodiosquez467@gmail.com>
2025-10-13 10:18:02 -0300

    feat(mem_punteros): Agrega resolución del ejercicio de memoria y punteros

---
+++ fi.uba.ar.cb100/src/examenes/parciales/junio2025/mem/Main.java
@@ -0,0 +1,45 @@
+package examenes.parciales.junio2025.mem;
+
+public class Main {
+    public static void main(String[] args) {
+        int X, Y, Z = 3;
+        char W;
+        int T;
+        int[] A = new int[4];
+        int[] B = A;
+
+        T = 77;
+        W = 'C';
+        X = T;
+        A[0] = X;
+        B[1] = A[0] / 2;
+
+        System.out.println(X + " - " + W + " - " + B[0] + " - " + A[1]);
+
+        W = (char) (T + 1);
+        B[2] = A[0] * 2;
+
+        System.out.println(W + " - " + T + " - " + B[1] + " - " + A[2]);
+
+        while (A[0] > 30) {
+            T -= 2;
+            X -= 3;
+            Y = X + Z;
+            System.out.println(Y + " - " + T + " - " + B[1] + " - " + Z);
+            Z = Y;
+            A[0] -= 10;
+        }
+
+        // ¿Qué hace el GC aquí? ¿Sobre qué variable?
+
+        /* Salida:
+            77 - C - 77 - 38
+            N - 77 - 38 - 154
+            77 - 75 - 38 - 3
+            148 - 73 - 38 - 77
+            216 - 71 - 38 - 148
+            281 - 69 - 38 - 216
+            343 - 67 - 38 - 281
+       */
+    }
+}
