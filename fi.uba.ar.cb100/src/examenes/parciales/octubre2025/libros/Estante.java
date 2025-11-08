9588ef24c16909e5ecef8dbbbe1a73faafb22680
pdiosquez467 <pablodiosquez467@gmail.com>
2025-11-08 15:17:15 -0300

    Agrega doc de la clase 'Estante', de la resolución del ejercicio 'Estante y Libros' - parcial octubre 2025.

--- fi.uba.ar.cb100/src/examenes/parciales/octubre2025/libros/Estante.java
+++ fi.uba.ar.cb100/src/examenes/parciales/octubre2025/libros/Estante.java
@@ -15,19 +15,24 @@ public class Estante {
     private final double pesoMaximoQueSoporta;
     private EstadoDelEstante estadoDelEstante;
 
-
     //ATRIBUTOS TRANSITORIOS ----------------------------------------------------------------------------------
     //CONSTRUCTORES -------------------------------------------------------------------------------------------
 
+    /**
+     * post: Inicializa un estante con el ancho y el peso máximo soportado
+     * indicados, sin libros asociados.
+     *
+     * @param ancho: el ancho del estante.
+     * @param pesoMaximoQueSoporta: el peso máximo que el estante soporta.
+     */
     public Estante(double ancho, double pesoMaximoQueSoporta) {
         Validaciones.validarNumeroMayorACero(ancho, "ancho");
-        Validaciones.validarNumeroMayorACero(pesoMaximoQueSoporta,"pesoMaximoQueSoporta");
+        Validaciones.validarNumeroMayorACero(pesoMaximoQueSoporta, "pesoMaximoQueSoporta");
         this.ancho = ancho;
         this.pesoMaximoQueSoporta = pesoMaximoQueSoporta;
         this.libros = new Vector<>();
         this.estadoDelEstante = EstadoDelEstante.OK;
     }
-
 
     //MÉTODOS ABSTRACTOS --------------------------------------------------------------------------------------
     //MÉTODOS HEREDADOS (CLASE)--------------------------------------------------------------------------------
@@ -36,17 +41,30 @@ public class Estante {
     //MÉTODOS GENERALES ---------------------------------------------------------------------------------------
     //MÉTODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
 
+    /**
+     * post: Agrega el libro indicado al estante. Si el libro excede
+     * el peso soportado por el estante, rompe el estante y lanza una excepción.
+     *
+     * @param libro: el libro a agregar.
+     * @throws RuntimeException si no hay espacio disponible.
+     */
     public void agregar(Libro libro) {
         Validaciones.validarNotNull(libro, "libro");
         this.validarEstadoOK();
         this.validarEspacioDisponible(libro.getAncho());
         if (this.excedeElPesoMaximoSoportado(libro)) {
             this.estadoDelEstante = EstadoDelEstante.ROTO;
-            return;
+            throw new RuntimeException("Estante ROTO");
         }
         this.libros.agregar(libro);
     }
 
+    /**
+     * post: Remueve el libro indicado del estante.
+     *
+     * @param libro: el libro a remover.
+     * @return el libro removido o null si no lo encontró.
+     */
     public Libro quitar(Libro libro) {
         Validaciones.validarNotNull(libro, "libro");
         this.validarEstadoOK();
@@ -54,10 +72,19 @@ public class Estante {
         return (indice == -1) ? null : this.libros.remover(indice);
     }
 
+    /**
+     * post: Modifica el estado del estante, reparándolo.
+     */
     public void reparar() {
         this.estadoDelEstante = EstadoDelEstante.OK;
     }
 
+    /**
+     * post: Busca el libro dado en el estante.
+     *
+     * @param libro: el libro a buscar.
+     * @return el libro buscado o null si no lo encontró.
+     */
     public Libro buscar(Libro libro) {
         Validaciones.validarNotNull(libro, "libro");
         this.validarEstadoOK();
@@ -70,11 +97,22 @@ public class Estante {
         return null;
     }
 
+    /**
+     * post: Devuelve la cantidad de libros que tiene el estante.
+     *
+     * @return la cantidad de libros que tiene el estante.
+     * @throws RuntimeException si el estante está ROTO.
+     */
     public int getCantidadDeLibros() {
         this.validarEstadoOK();
         return this.libros.size();
     }
 
+    /**
+     * post: Devuelve el peso actual soportado por el estante.
+     * @return el peso actual soportado por el estante.
+     * @throws RuntimeException si el estante está ROTO.
+     */
     public double getPesoActual() {
         this.validarEstadoOK();
         double pesoActual = 0;
@@ -85,6 +123,11 @@ public class Estante {
         return pesoActual;
     }
 
+    /**
+     * post: Devuelve el espacio ocupado por los libros.
+     * @return el espacio ocupado.
+     * @throws RuntimeException si el estante está ROTO.
+     */
     public double getEspacioOcupado() {
         this.validarEstadoOK();
         double espacioOcupado = 0;
@@ -95,11 +138,20 @@ public class Estante {
         return espacioOcupado;
     }
 
+    /**
+     * post: Devuelve el espacio libre disponible en el estante.
+     * @return el espacio libre.
+     * @throws RuntimeException si el estante está ROTO.
+     */
     public double getEspacioLibre() {
         this.validarEstadoOK();
         return this.ancho - this.getEspacioOcupado();
     }
 
+    /**
+     * post: Indica si el estante está OK.
+     * @return verdadero si el estante está OK.
+     */
     public boolean estaOK() {
         return this.estadoDelEstante.equals(EstadoDelEstante.OK);
     }
@@ -110,6 +162,11 @@ public class Estante {
     //GETTERS COMPLEJOS ---------------------------------------------------------------------------------------
 
     //GETTERS SIMPLES -----------------------------------------------------------------------------------------
+
+    /**
+     * post: Devuelve un arreglo con los libros del estante.
+     * @return un arreglo con los libros guardados.
+     */
     public Libro[] getLibros() {
         Libro[] librosGuardados = new Libro[this.libros.size()];
         for (int i = 0; i < this.libros.size(); i++) {
@@ -118,10 +175,18 @@ public class Estante {
         return librosGuardados;
     }
 
+    /**
+     * post: Devuelve el ancho del estante.
+     * @return el ancho del estante.
+     */
     public double getAncho() {
         return ancho;
     }
 
+    /**
+     * post: Devuelve el peso máximo que soporta el estante.
+     * @return el peso máximo soportado.
+     */
     public double getPesoMaximoQueSoporta() {
         return pesoMaximoQueSoporta;
     }
