022d1d6bac9369e6ddde1cdaeda4a74cf438f6a1
pdiosquez467 <pablodiosquez467@gmail.com>
2025-11-10 21:20:22 -0300

    Agrega resolución documentada del ejercicio 'Administrador de imágenes'

---
+++ fi.uba.ar.cb100/src/examenes/parciales/listas/repaso/imagenes/AdministradorDeImagenes.java
@@ -0,0 +1,52 @@
+package examenes.parciales.listas.repaso.imagenes;
+
+import java.util.List;
+
+import validaciones.Validaciones;
+
+public class AdministradorDeImagenes {
+
+    /**
+     * post: Busca la imagen con más pixeles puros del listado de imágenes dado.
+     * Un pixel es 'puro' si al menos dos de sus tres componentes RGB son iguales a cero.
+     */
+    public Imagen2D getImagenMasPura(List<Imagen2D> imagenes) {
+        Validaciones.validarNotNull(imagenes, "imágenes");
+        Imagen2D imagenMasPuraActual = null;
+        for (Imagen2D imagen: imagenes) {
+            if (imagen == null) continue;
+            if ((imagenMasPuraActual == null) ||
+                    (getCantidadDePixelesPuros(imagen) > getCantidadDePixelesPuros(imagenMasPuraActual))) {
+                imagenMasPuraActual = imagen;
+            }
+        }
+        return imagenMasPuraActual;
+    }
+
+    /**
+     * post: Devuelve la cantidad de pixeles puros de la imagen dada.
+     */
+    public int getCantidadDePixelesPuros(Imagen2D imagen) {
+        Validaciones.validarNotNull(imagen, "imagen");
+        int totalPixelesPuros = 0;
+        for (int i = 1; i <= imagen.getAncho(); i++) {
+            for (int j = 1; j <= imagen.getAlto(); j++) {
+                Pixel pixel = imagen.getPixel(i, j);
+                if (esPixelPuro(pixel)) totalPixelesPuros++;
+            }
+        }
+        return totalPixelesPuros;
+    }
+
+    /**
+     * post: Indica si el pixel dado es puro.
+     */
+    public boolean esPixelPuro(Pixel pixel) {
+        Validaciones.validarNotNull(pixel, "pixel");
+        int totalComponentesPuras = 0;
+        if (pixel.getRojo() == 0) totalComponentesPuras++;
+        if (pixel.getVerde() == 0) totalComponentesPuras++;
+        if (pixel.getAzul() == 0) totalComponentesPuras++;
+        return totalComponentesPuras >= 2;
+    }
+}
